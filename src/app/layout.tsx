6f30d9ed976a8569e1a78be74722cf01aa28462d
Gopi Krishna E D <gopikrishnaed6@gmail.com>
2025-11-09 13:22:20 +0000

    make the background starry and is a representation of universe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased starry-background">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
         {children}
         <Toaster />
       </body>
