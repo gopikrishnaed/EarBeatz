b2628022bec5c1d17b38dee58cf7f3292ac30b54
Gopi Krishna E D <gopikrishnaed6@gmail.com>
2025-11-09 12:53:18 +0000

    Error: Invalid src prop (https://zmwvinypbdpcxlzaadnb.supabase.co/storag

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'zmwvinypbdpcxlzaadnb.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
